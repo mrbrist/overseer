374734885d647dceeb86e6339bcd9e5f84652f08
Unknown <bristol2801@gmail.com>
2019-05-18 20:26:21 +0200

    Major design improvements

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { menubar } = require('@amaurymartiny/menubar');
 const mb = menubar({
   height: 550,
   width: 450,
-  icon: 'assets/logo_32.png'
+  icon: 'assets/logo_32.png',
+  resizable: false
 });
 
 mb.on('ready', () => {
--- src/renderer.js
+++ src/renderer.js
@@ -1,26 +1,34 @@
-// // This file is required by the index.html file and will
-// // be executed in the renderer process for that window.
-// // All of the Node.js APIs are available in this process.
+// Include and setup packages
 const {clipboard} = require('electron');
-const remote = require('electron').remote;
-const app = remote.app;
-var Datastore = require('nedb'),
-  db = new Datastore({ filename: app.getPath('userData') + '/data', autoload: true });
+const $ = require('jquery');
+
+// Setup date and store
+var globalDB = []
+
+// String escape function
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
 
 
-function addSlashes(str) {
-  return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-}
+
 // Start watching clipboard for changes
-var oldClip = addSlashes(clipboard.readText());
+var oldClip = clipboard.readText();
 setInterval(function() {
-  var newClip = addSlashes(clipboard.readText());
+  var newClip = clipboard.readText();
   if (newClip !== oldClip) {
     oldClip = newClip
-    db.insert({oldClip}, function (err, newDoc) {
-      db.find({}, function (err, docs) {
-        console.log(docs);
-      });
-    });
+    globalDB.unshift([oldClip, new Date().toString().slice(16,21)])
+    console.log(globalDB);
+    document.getElementById('clipList').innerText = ""
+    for (var i = 0; i < globalDB.length; i++) {
+      var content = `<div class="clipItem"><span id="clipPreview">${escapeHtml(globalDB[i][0])}</span><span id="clipTime">${globalDB[i][1]}</span></div>`
+      $("#clipList").append(content);
+    }
   }
 }, 1);
