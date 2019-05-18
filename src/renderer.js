2c15c6aaaf6ea3a0a6d8dcc4d7325225d0f06ced
Unknown <bristol2801@gmail.com>
2019-05-18 14:24:50 +0200

    rename repo

--- src/renderer.js
+++ src/renderer.js
@@ -2,13 +2,25 @@
 // // be executed in the renderer process for that window.
 // // All of the Node.js APIs are available in this process.
 const {clipboard} = require('electron');
-// var content = clipboard.readText();
-// alert(content);
-var oldClip = clipboard.readText();
-setInterval(function(){
-  var newClip = clipboard.readText();
+const remote = require('electron').remote;
+const app = remote.app;
+var Datastore = require('nedb'),
+  db = new Datastore({ filename: app.getPath('userData') + '/data', autoload: true });
+
+
+function addSlashes(str) {
+  return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+// Start watching clipboard for changes
+var oldClip = addSlashes(clipboard.readText());
+setInterval(function() {
+  var newClip = addSlashes(clipboard.readText());
   if (newClip !== oldClip) {
     oldClip = newClip
-    console.log(oldClip)
+    db.insert({oldClip}, function (err, newDoc) {
+      db.find({}, function (err, docs) {
+        console.log(docs);
+      });
+    });
   }
 }, 1);
