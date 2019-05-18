4070a501f0540a2f05816ae368b3b0ddacb1b754
Unknown <bristol2801@gmail.com>
2019-05-18 12:57:14 +0200

    Added clipboard watching and fixed bug
    Fixed a bug where the app would not open on windows

--- main.js
+++ main.js
@@ -1,70 +1,12 @@
-const {app, BrowserWindow, ipcMain, Tray} = require('electron');
-const path = require('path');
+const { menubar } = require('@amaurymartiny/menubar');
 
-let tray = undefined
-let window = undefined
+const mb = menubar({
+  height: 550,
+  width: 450,
+  icon: 'assets/logo_16.png'
+});
 
-// Don't show the app in the doc
-app.dock.hide()
-
-app.on('ready', () => {
-  createTray()
-  createWindow()
-})
-
-const createTray = () => {
-  tray = new Tray('assets/logo_16.png')
-  tray.on('click', function (event) {
-    toggleWindow()
-  });
-}
-
-const getWindowPosition = () => {
-  const windowBounds = window.getBounds();
-  const trayBounds = tray.getBounds();
-
-  // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
-
-  // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4);
-
-  return {x: x, y: y};
-}
-
-const createWindow = () => {
-  window = new BrowserWindow({
-    width: 320,
-    height: 450,
-    show: false,
-    frame: false,
-    fullscreenable: false,
-    resizable: false,
-    transparent: false,
-    webPreferences: {
-      backgroundThrottling: false
-    }
-  })
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
-
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
-    }
-  })
-}
-
-const toggleWindow = () => {
-  window.isVisible() ? window.hide() : showWindow();
-}
-
-const showWindow = () => {
-  const position = getWindowPosition();
-  window.setPosition(position.x, position.y, false);
-  window.show();
-}
-
-ipcMain.on('show-window', () => {
-  showWindow()
-})
+mb.on('ready', () => {
+  console.log('app is ready');
+  // your app code here
+});
--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,14 @@
 // // This file is required by the index.html file and will
 // // be executed in the renderer process for that window.
 // // All of the Node.js APIs are available in this process.
+const {clipboard} = require('electron');
+// var content = clipboard.readText();
+// alert(content);
+var oldClip = clipboard.readText();
+setInterval(function(){
+  var newClip = clipboard.readText();
+  if (newClip !== oldClip) {
+    oldClip = newClip
+    console.log(oldClip)
+  }
+}, 1);
