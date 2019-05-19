d250079854bcc42da5b37a2e16624e7a03f61ef8
Brist <bristol2801@gmail.com>
2019-05-19 18:49:12 +0200

    Update renderer.js

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 // Include and setup packages
 const {clipboard} = require('electron');
 const $ = require('jquery');
+const uuidv1 = require('uuid/v1');
 
 // Setup date and store
 var globalDB = []
@@ -13,21 +14,32 @@ function escapeHtml(unsafe) {
          .replace(/>/g, "&gt;")
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
- }
+}
 
+// Events from client
+$('#clearBtn').on( "click", function() {
+  // console.log("CLICKED");
+  globalDB = []
+  $("#clipList").html("<span id='noClips'>No clipboard history :(</span>")
+});
 
+// When the user clicks a clip it will be moved to the clipboard
+$('#clipList').on("click", ".clipItem", function(){
+  // Gets UUID element text
+  var clipUUID = $(this).children()[0].innerText
+  var clipFromArray = globalDB.find(clip => clip[0] === clipUUID)[1];
+  clipboard.writeText(clipFromArray)
+});
 
 // Start watching clipboard for changes
-var oldClip = clipboard.readText();
 setInterval(function() {
   var newClip = clipboard.readText();
-  if (newClip !== oldClip) {
-    oldClip = newClip
-    globalDB.unshift([oldClip, new Date().toString().slice(16,21)])
-    console.log(globalDB);
+  if (globalDB.find(clip => clip[1] === newClip) === undefined) {
+    globalDB.unshift([uuidv1(), newClip, new Date().toString().slice(16,21)])
+    // console.log(globalDB);
     document.getElementById('clipList').innerText = ""
     for (var i = 0; i < globalDB.length; i++) {
-      var content = `<div class="clipItem"><span id="clipPreview">${escapeHtml(globalDB[i][0])}</span><span id="clipTime">${globalDB[i][1]}</span></div>`
+      var content = `<div class="clipItem"><span id="uuid" style="display: none">${globalDB[i][0]}</span><span id="clipPreview">${escapeHtml(globalDB[i][1])}</span><span id="clipTime">${globalDB[i][2]}</span></div>`
       $("#clipList").append(content);
     }
   }
